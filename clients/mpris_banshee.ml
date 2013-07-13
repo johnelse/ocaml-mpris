f90604e27c28825e50efce6f5283ea9c9c0589ca
John Else <john.else@citrix.com>
2013-07-13 15:58:27 +0100

    Rename Mpris_client to Mpris_client_generic
    Signed-off-by: John Else <john.else@citrix.com>

--- clients/mpris_banshee.ml
+++ clients/mpris_banshee.ml
@@ -2,10 +2,10 @@ let make_proxy () =
   Mpris_proxy.make "org.mpris.MediaPlayer2.banshee"
 
 module Org_mpris_MediaPlayer2 =
-  Mpris_client.Org_mpris_MediaPlayer2
+  Mpris_client_generic.Org_mpris_MediaPlayer2
 
 module Org_mpris_MediaPlayer2_Player =
-  Mpris_client.Org_mpris_MediaPlayer2_Player
+  Mpris_client_generic.Org_mpris_MediaPlayer2_Player
 
 module Org_mpris_MediaPlayer2_Playlists =
-  Mpris_client.Org_mpris_MediaPlayer2_Playlists
+  Mpris_client_generic.Org_mpris_MediaPlayer2_Playlists
